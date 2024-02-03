3b06bf5b7d5309e1cef770ffa2eb78d9e61bbc02
kindarkhediyaRavi <157191309+kindarkhediyaRavi@users.noreply.github.com>
2024-02-03 18:30:51 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multiCast
+{
+    public delegate void d1();
+    internal class Program
+    {
+        static void f1() { Console.WriteLine("First..."); }
+        static void f2() { Console.WriteLine("Second..."); }
+        static void f3() { Console.WriteLine("Third..."); }
+
+        static void Main(string[] args)
+        {
+            d1 obj = new d1(f1);
+            obj();
+
+            Console.WriteLine("\n After Add f2 and f3");
+
+            obj += new d1(f2);  // add f2 and f3 method  
+            obj += new d1(f3);
+            obj();
+            Console.WriteLine("\n After f2 Remove");
+            obj -= new d1(f2); // sub f2
+            obj();
+            Console.ReadLine();
+        }
+    }
+}
